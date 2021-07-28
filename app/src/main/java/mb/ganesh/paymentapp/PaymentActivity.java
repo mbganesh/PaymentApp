5e9c336a1dfba5e7509b1674245cdfd38cf6e8fa
SharedPreferences <boothaganesh.jdev@gmail.com>
2021-07-28 16:35:40 +0530

    Phase1 with rayzorpay

--- app/src/main/java/mb/ganesh/paymentapp/PaymentActivity.java
+++ app/src/main/java/mb/ganesh/paymentapp/PaymentActivity.java
@@ -1,8 +1,10 @@
 package mb.ganesh.paymentapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -19,10 +21,12 @@ public class PaymentActivity extends AppCompatActivity {
     private String KeySecret = "NbbjnMUllUtABJaFxZG2Mc1c";
 
     MaterialButton buyNowBtn;
-    MaterialCardView card1mo , card3mo , card12mo;
+    MaterialCardView card1mo, card3mo, card12mo;
     MaterialCardView tempCard;
     String valueToPay = "101";
     int amount;
+    boolean isCardSelected = false;
+    ProgressDialog progress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +41,7 @@ public class PaymentActivity extends AppCompatActivity {
         //init values
 
         amount = Math.round(Float.parseFloat(valueToPay));
+        buyNowBtn.setVisibility(View.GONE);
 
 
 
@@ -45,7 +50,8 @@ public class PaymentActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 tempCard = card1mo;
-                hideOtherStuff(tempCard , card3mo , card12mo);
+                hideOtherStuff(tempCard, card3mo, card12mo);
+                buyNowBtn.setVisibility(View.VISIBLE);
             }
         });
 
@@ -53,7 +59,8 @@ public class PaymentActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 tempCard = card3mo;
-                hideOtherStuff(tempCard , card1mo , card12mo);
+                hideOtherStuff(tempCard, card1mo, card12mo);
+                buyNowBtn.setVisibility(View.VISIBLE);
             }
         });
 
@@ -61,16 +68,39 @@ public class PaymentActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 tempCard = card12mo;
-                hideOtherStuff(tempCard , card1mo , card3mo);
+                hideOtherStuff(tempCard, card1mo, card3mo);
+                buyNowBtn.setVisibility(View.VISIBLE);
             }
         });
+
 
         buyNowBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    rayzorPayInit(amount);
+                startLoader();
+                rayzorPayInit(amount);
+
             }
         });
+    }
+
+    private void startLoader() {
+        progress = new ProgressDialog(this);    //ProgressDialog
+        progress.setTitle("Loading");
+        progress.setMessage("Please wait");
+        progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progress.setCancelable(false);
+        progress.show();
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    Thread.sleep(7000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                progress.dismiss();
+            }
+        }).start();
     }
 
     private void rayzorPayInit(int amount) {
@@ -80,24 +110,29 @@ public class PaymentActivity extends AppCompatActivity {
         JSONObject jsonObject = new JSONObject();
 
         try {
-            jsonObject.put("name" , "GilGal");
-            jsonObject.put("description" , "GilGal online payment");
-            jsonObject.put("theme.color" , "#185ADB");
-            jsonObject.put("currency" , "INR");
-            jsonObject.put("amount" , amount);
-            jsonObject.put("prefill.contact" , "9876543210");
-            jsonObject.put("prefill.email" , "gilgal.payment@gmail.com");
-            checkout.open(PaymentActivity.this , jsonObject);
-        }catch (Exception e){
+            jsonObject.put("name", "GilGal");
+            jsonObject.put("description", "GilGal online payment");
+            jsonObject.put("theme.color", "#185ADB");
+            jsonObject.put("currency", "INR");
+            jsonObject.put("amount", amount);
+            jsonObject.put("prefill.contact", "9876543210");
+            jsonObject.put("prefill.email", "gilgal.payment@gmail.com");
+            checkout.open(PaymentActivity.this, jsonObject);
+
+
+        } catch (Exception e) {
             e.getStackTrace();
         }
     }
 
     private void hideOtherStuff(MaterialCardView clickedCard, MaterialCardView card1, MaterialCardView card2) {
-        clickedCard.setStrokeColor(ContextCompat.getColor(PaymentActivity.this , R.color.mBlue));
+        clickedCard.setStrokeColor(ContextCompat.getColor(PaymentActivity.this, R.color.mBlue));
         clickedCard.setStrokeWidth(3);
         card1.setStrokeWidth(0);
         card2.setStrokeWidth(0);
 
+        isCardSelected = true;
     }
 }
+
+
